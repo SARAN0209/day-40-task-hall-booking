d94e2ef0396332e5ffbcc4bbbad96e215d37aadf
Saran <saranbabu2609@gmail.com>
2022-12-12 19:37:51 +0530

    second commit

--- connect.js
+++ connect.js
@@ -9,6 +9,7 @@ module.exports={
             console.log(process.env.MONGO)
             const client =await MongoClient.connect(process.env.MONGO);
             this.selectedDb=client.db('hallbooking');
+            console.log('connection is established')
         }
         catch (err){
             console.log(err);
